080fe0f9fb56d5553f5fd5466d8e7078c79f2f4b
Jordi Jordana <jordi.jordana.1988@gmail.com>
2019-11-12 21:05:19 +0100

    Exercises done

--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -1,16 +1,6 @@
-"""
-You are presented with an integer number larger than 5. Your goal is to identify the longest side
-possible in a right triangle whose sides are not longer than the number you are given.
+"""The solution in this case is about using list comprehensions to make the code
+more efficient and simple"""
 
-For example, if you are given the number 15, there are 3 possibilities to compose right triangles:
-
-1. [3, 4, 5]
-2. [6, 8, 10]
-3. [5, 12, 13]
-
-The following function shows one way to solve the problem but the code is not ideal or efficient.
-Refactor the code based on what you have learned about code simplicity and efficiency.
-"""
 
 X = int(input("What is the maximal length of the triangle side? Enter a number: "))
 
